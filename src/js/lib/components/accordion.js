9c49a35d06a74d733bdc95e11b0c30e07577b252
Oleg Sazonov <oleg.sazonov.work@gmail.com>
2025-01-05 17:08:49 +0200

    feat(accordion.js): add accordion-func

---
+++ src/js/lib/components/accordion.js
@@ -0,0 +1,20 @@
+'use strict';
+
+import $ from '../core';
+
+$.prototype.accordion = function(headActive = 'accordion-head_active', contentActive = 'accordion-content_active', paddings = 40) {
+	for (let i = 0; i < this.length; i++) {
+		$(this[i]).click(() => {
+			$(this[i]).toggleClass(headActive);
+			$(this[i].nextElementSibling).toggleClass(contentActive);
+
+			if (this[i].classList.contains(headActive)) {
+				this[i].nextElementSibling.style.maxHeight = this[i].nextElementSibling.scrollHeight + paddings + "px";	
+			} else {
+				this[i].nextElementSibling.style.maxHeight = "0px";
+			}
+		});
+	}
+}
+
+$('.accordion-head').accordion();
