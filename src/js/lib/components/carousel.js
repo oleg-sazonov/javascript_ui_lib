ef399568686ef883a2620252a2491e059943bffc
Oleg Sazonov <oleg.sazonov.work@gmail.com>
2025-01-08 21:50:34 +0200

    feat(carousel.js): add carousel-func

--- src/js/lib/components/carousel.js
+++ src/js/lib/components/carousel.js
@@ -6,9 +6,67 @@ $.prototype.carousel = function() {
 	for (let i = 0; i < this.length; i++) {
 		const width = window.getComputedStyle(this[i].querySelector('.carousel-inner')).width;
 		const slides = this[i].querySelectorAll('.carousel-item');
-		this[i].querySelector('.carousel-slides').style.width = 100 * slides.length + '%'; // 3 slides = 300%
+		const slidesField = this[i].querySelector('.carousel-slides');
+		const dots = this[i].querySelectorAll('.carousel-indicators li');
+
+		slidesField.style.width = 100 * slides.length + '%'; // 3 slides = 300%
 		slides.forEach(slide => {
 			slide.style.width = width;
+		});
+
+		let offset = 0;
+		let slideIndex = 0;
+
+		$(this[i].querySelector('[data-slide="next"]')).click((e) => { // click on arrow
+			e.preventDefault();
+			if (offset == (+width.replace(/\D/g, '') * (slides.length - 1))) {
+				offset = 0; // if current slide is the last one
+			} else {
+				offset += +width.replace(/\D/g, ''); // add width of slide
+			}
+
+			slidesField.style.transform = `translateX(-${offset}px)`;
+
+			if (slideIndex == slides.length - 1) {
+				slideIndex = 0;
+			} else {
+				slideIndex++;
+			}
+
+			dots.forEach(dot => dot.classList.remove('active'));
+			dots[slideIndex].classList.add('active');
+		});
+
+		$(this[i].querySelector('[data-slide="prev"]')).click((e) => { // click on arrow
+			e.preventDefault();
+			if (offset == 0) {
+				offset = +width.replace(/\D/g, '') * (slides.length - 1);
+			} else {
+				offset -= +width.replace(/\D/g, ''); // add width of slide
+			}
+
+			slidesField.style.transform = `translateX(-${offset}px)`;
+
+			if (slideIndex == 0) {
+				slideIndex = slides.length - 1;
+			} else {
+				slideIndex--;
+			}
+
+			dots.forEach(dot => dot.classList.remove('active'));
+			dots[slideIndex].classList.add('active');
+		});
+
+		const sliderId = this[i].getAttribute('id');
+		$(`#${sliderId} .carousel-indicators li`).click((e) => {
+			const slideTo = e.target.getAttribute('data-slide-to');
+
+			slideIndex = slideTo;
+			offset = +width.replace(/\D/g, '') * slideTo;
+
+			slidesField.style.transform = `translateX(-${offset}px)`;
+			dots.forEach(dot => dot.classList.remove('active'));
+			dots[slideIndex].classList.add('active');
 		});
 	}
 }
--- src/js/main.js
+++ src/js/main.js
@@ -46,17 +46,3 @@ $('#trigger').click(() => $('#trigger').createModal({
 		]
 	}
 }));
-
-// $('.wrap').html(`
-// 	<div class="dropdown">
-// 		<button class="btn btn-primary dropdown-toggle" id="dropdownMenuButton">Dropdown Button</button>
-// 		<div class="dropdown-menu" data-toggle-id="dropdownMenuButton">
-// 			<a href="#" class="dropdown-item">Action</a>
-// 			<a href="#" class="dropdown-item">Action #2</a>
-// 			<a href="#" class="dropdown-item">Action #3</a>
-// 		</div>
-// 	</div>
-// `);
-
-// $('.dropdown-toggle').dropdown();
-
